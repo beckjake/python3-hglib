c6e3a85c93f0ed096018fa878e73cf3a4c59d85b
Idan Kamara <idankk86@gmail.com>
2011-08-19 20:14:15 +0300

    hglib: change import style

--- hglib/__init__.py
+++ hglib/__init__.py
@@ -1,4 +1,4 @@
-from client import hgclient
+import client
 
 HGPATH = 'hg'
 
@@ -6,4 +6,4 @@ def open(path=None, encoding=None, configs=None):
     ''' starts a cmdserver for the given path (or for a repository found in the
     cwd). HGENCODING is set to the given encoding. configs is a list of key, value,
     similar to those passed to hg --config. '''
-    return hgclient(path, encoding, configs)
+    return client.hgclient(path, encoding, configs)
