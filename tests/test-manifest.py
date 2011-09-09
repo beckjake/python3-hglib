d7e7647eb1e7b9e16af08a7dc86997bebcdefc08
Idan Kamara <idankk86@gmail.com>
2011-09-09 19:10:02 +0300

    test-manifest: Windows doesn't have symlink or executable bit

--- tests/test-manifest.py
+++ tests/test-manifest.py
@@ -3,16 +3,23 @@ import common, hglib, os, stat
 class test_manifest(common.basetest):
     def test_basic(self):
         self.append('a', 'a')
-        self.append('b', 'b')
-        os.chmod('b', os.stat('b')[0] | stat.S_IEXEC)
-        os.symlink('b', 'c')
+        files = ['a']
+        manifest = [('047b75c6d7a3ef6a2243bd0e99f94f6ea6683597', '644', False,
+                     False, 'a')]
+
+        if os.name == 'posix':
+            self.append('b', 'b')
+            os.chmod('b', os.stat('b')[0] | stat.S_IEXEC)
+            os.symlink('b', 'c')
+
+            files.extend(['b', 'c'])
+            manifest.extend([('62452855512f5b81522aa3895892760bb8da9f3f', '755',
+                              True, False, 'b'),
+                             ('62452855512f5b81522aa3895892760bb8da9f3f', '644',
+                              False, True, 'c')])
+
         self.client.commit('first', addremove=True)
 
-        self.assertEquals(list(self.client.manifest(all=True)), ['a', 'b', 'c'])
-
-        manifest = \
-          [('047b75c6d7a3ef6a2243bd0e99f94f6ea6683597', '644', False, False, 'a'),
-           ('62452855512f5b81522aa3895892760bb8da9f3f', '755', True, False, 'b'),
-           ('62452855512f5b81522aa3895892760bb8da9f3f', '644', False, True, 'c')]
+        self.assertEquals(list(self.client.manifest(all=True)), files)
 
         self.assertEquals(list(self.client.manifest()), manifest)
