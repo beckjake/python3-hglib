d148e88cb1db796b648354fcdf5c1dcad87b85b0
Idan Kamara <idankk86@gmail.com>
2011-12-22 19:12:47 +0200

    hglib: add top level clone method

--- hglib/__init__.py
+++ hglib/__init__.py
@@ -20,3 +20,19 @@ def init(dest=None, ssh=None, remotecmd=None, insecure=False,
         raise error.CommandError(args, proc.returncode, out, err)
 
     return client.hgclient(dest, encoding, configs, connect=False)
+
+def clone(source=None, dest=None, noupdate=False, updaterev=None, rev=None,
+          branch=None, pull=False, uncompressed=False, ssh=None, remotecmd=None,
+          insecure=False, encoding=None, configs=None):
+    args = util.cmdbuilder('clone', source, dest, noupdate=noupdate,
+                           updaterev=updaterev, rev=rev, branch=branch,
+                           pull=pull, uncompresses=uncompressed,
+                           e=ssh, remotecmd=remotecmd, insecure=insecure)
+
+    args.insert(0, HGPATH)
+    proc = util.popen(args)
+    out, err = proc.communicate()
+    if proc.returncode:
+        raise error.CommandError(args, proc.returncode, out, err)
+
+    return client.hgclient(dest, encoding, configs, connect=False)
